d0c9350cc4c3e200482d281ba54f79ba50757a1b
od <65794818+forbeso@users.noreply.github.com>
2022-12-12 05:00:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Od Forbes
+#The Ultimate Wacky Recipe Maker
+
+foodType = input("Enter type of food: \n")
+plantType = input("Enter type of plant: \n")
+cookingMethod = input("Cooking Method: \n")
+burnt = input("Enter word for burned food: ")
+
+householdItem = input("Household Item: \n")
+
+print("Creating wackiness...")
+
+print(cookingMethod,foodType, "with",burnt, plantType, "on a bed of", householdItem)
